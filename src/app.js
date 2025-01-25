4979b72a6aac4de2d574210751ccd5cbcb712604
Shadab Akhtar <tempryderop@gmail.com>
2025-01-26 00:18:05 +0530

    Data Sanitization in user schema

--- src/app.js
+++ src/app.js
@@ -3,16 +3,73 @@ const connectDB = require('./connection/database');
 const User = require('./model/user');
 const app = express();
 
+app.use(express.json());
+
+app.get('/user', async (req, res) => {
+  const email = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: email });
+    if (users.length > 0) {
+      res.send(users);
+    } else {
+      res.status(404).send('user not found');
+    }
+  } catch (error) {
+    res.status(400).send('Something went Wrong');
+  }
+});
+
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send('Something went Wrong');
+  }
+});
+
+app.patch('/user', async (req, res) => {
+  try {
+    const id = req.body.id;
+    const user = await User.findByIdAndUpdate(
+      { _id: req.body.id },
+      { firstName: 'Put Request' },
+      {
+        returnDocument: 'after',
+      },
+      {
+        runValidators: true,
+      }
+    );
+    console.log(user);
+    if (user) {
+      res.send(user);
+    } else {
+      throw new Error('User not found with Id');
+    }
+  } catch (error) {
+    res.status(400).send('User not found with Id');
+  }
+});
+
+app.delete('/user', async (req, res) => {
+  try {
+    const id = req.body.id;
+    const user = await User.findByIdAndDelete({ _id: req.body.id });
+    console.log(user);
+    if (user) {
+      res.send(user);
+    } else {
+      throw new Error('User not found with Id');
+    }
+  } catch (error) {
+    res.status(400).send('User not found with Id');
+  }
+});
+
 app.post('/signup', async (req, res) => {
-  const userDetails = {
-    firstName: 'Sachin',
-    lastName: 'Tendulkar',
-    emailId: 'Sachin@gmail.com',
-    age: 57,
-    password: 'Sachin',
-    gender: 'Male',
-  };
-  const user = new User(userDetails);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send('data saved successfully');
--- src/model/user.js
+++ src/model/user.js
@@ -1,26 +1,54 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!['male', 'female', 'others'].includes(value)) {
+          throw new Error('Invalid Gender');
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: 'https://google.com',
+    },
+    about: {
+      type: String,
+      default: 'This is about description',
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
